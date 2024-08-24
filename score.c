5e869e17c67d54aba45adf884821c7ab16b96c42
Deepak Chandhru N <122144025+Deepakchandhru@users.noreply.github.com>
2024-08-24 19:14:24 +0530

    Update score.c

--- score.c
+++ score.c
@@ -3,16 +3,17 @@
 
 struct noderec{
 int info;
+char isout;
 struct noderec *next;
 };
 typedef struct noderec *node;
 
-node start,end,new,temp,prev;
+node new,temp;
 typedef node header;
 header h;
 
 void create(){
-h=(header) malloc(sizeof(header));
+h=(header) malloc(sizeof(struct noderec));
 h->next=h;
 }
 
@@ -20,9 +21,10 @@ int isEmpty(){
 return(h->next==h);
 }
 
-void insert(int x){
-    new=(node) malloc(sizeof(node));
+void insert(int x,char out){
+    new=(node) malloc(sizeof(struct noderec));
     new->info=x;
+    new->isout=out;
     temp=h;
     while(temp->next!=h){
         temp=temp->next;
@@ -48,18 +50,39 @@ void totalmatches(){
 void scorehistory(){
     if(!isEmpty()){
         totalmatches();
-        temp=h->next;
-        int i=1;
-        while(temp!=h){
-            printf("Match:%d \t\t Score: %d \n",i,temp->info);
-            temp=temp->next;
+        temp = h->next;
+        int totalruns = 0;
+        int totalinnings = 0;
+        int i = 1;
+        while(temp != h){
+            printf("Match:%d \t\t Score: %d", i, temp->info);
+            if(temp->isout=='N'){
+                printf("*");
+            }else{ 
+                totalinnings++;
+            }
+            printf("\n");
+            totalruns += temp->info;
+            temp = temp->next;
             i++;
         }
+
+        printf("\n");
+
+        if(totalinnings == 0){
+            printf("Average Score: %.2f\n", (float)totalruns / 1.00);
+        }else{
+            printf("Average Score: %.2f\n", (float)totalruns / (float)totalinnings);
+        }
+
+        printf("No.of Unbeaten Knocks: %d\n", i - totalinnings - 1);
+
         printf("\n");
     }else{
         printf("Please play the game at least once .\n");
     }
 }
+
 
 int highscore(){
     if(!isEmpty()){
@@ -77,4 +100,3 @@ int highscore(){
         return 0;
     }
 }
-
